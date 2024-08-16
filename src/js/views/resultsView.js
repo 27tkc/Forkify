45f7dfd5ae6e1bc6a5fc6133251d317cf625286b
Tarun Kumar Choudhary <tarunkc22@gmail.com>
2024-08-15 21:34:53 -0400

    Adding Pagination and Architecture

---
+++ src/js/views/resultsView.js
@@ -0,0 +1,31 @@
+import View from "./View.js";
+import icons from "url:../../img/icons.svg";
+class ResultsView extends View {
+  _parentElement = document.querySelector(".results");
+  _errorMessage = "No recipes found! Please try again.";
+  _message = "";
+
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join("");
+  }
+  _generateMarkupPreview(result) {
+    return `
+    <li class="preview">
+            <a class="preview__link preview__link--active" href="#${result.id}">
+              <figure class="preview__fig">
+                <img src="${result.image}" alt="${result.title}" />
+              </figure>
+              <div class="preview_result">
+                <h4 class="preview__title">${result.title}</h4>
+                <p class="preview__publisher">${result.publisher}</p>
+                <div class="preview__user-generated">
+                  <svg>
+                    <use href="${icons}#icon-user"></use>
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </li>`;
+  }
+}
+export default new ResultsView();
